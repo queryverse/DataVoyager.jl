51a512a1f431696b2a3649c4e0309100d32a9138
David Anthoff <anthoff@berkeley.edu>
2017-12-31 10:31:58 -0800

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,14 @@
 using DataVoyager
+using NamedTuples
+using Blink
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@testset "DataVoyager" begin
+
+source = [@NT(a=1,b=1), @NT(a=2,b=2)]
+
+v = Voyager(source)
+
+@test typeof(v.w) == AtomShell.Window
+
+end
