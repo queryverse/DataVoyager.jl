57c2aac8d265e8cbc3dc345674f4debae4ecb815
David Anthoff <anthoff@berkeley.edu>
2018-01-05 16:05:17 -0800

    Replace JSON story with something simpler

--- src/DataVoyager.jl
+++ src/DataVoyager.jl
@@ -11,57 +11,10 @@ mutable struct Voyager
     w
     function Voyager()
         w = Blink.Window()
-        
+
         loadfile(w, joinpath(@__DIR__, "htmlui", "main.html"))
 
         new(w)
-    end
-end
-
-@generated function format_iterable_table_as_json(it::T) where T
-    ET = eltype(T)
-
-    col_names = fieldnames(ET)
-    col_types = [ET.parameters[i] for i in 1:length(col_names)]
-
-    row_output = Expr(:block)
-
-    for (i,v) in enumerate(col_names)
-        if i>1
-            push!(row_output.args, :(print(buf, ", ")))
-            first_column = false
-        end
-        push!(row_output.args, :(print(buf, "\"")))
-        push!(row_output.args, :(print(buf, $("$v"))))
-        push!(row_output.args, :(print(buf, "\": ")))
-        if col_types[i] <: DataValue
-            push!(row_output.args, quote
-                if isnull(row[$i])
-                    print(buf, "null")
-                else
-                    JSON.print(buf, get(row[$i]))
-                end
-            end)
-        else
-            push!(row_output.args, :(JSON.print(buf, row[$i])))
-        end
-    end
-
-    quote
-        buf = IOBuffer()
-        print(buf, "{\"values\":[")
-        for (i,row) in enumerate(it)
-            if i>1
-                print(buf, ",")
-                first_row = false
-            end
-            print(buf, "{")
-            $(row_output)
-            print(buf, "}")
-        end
-        print(buf, "]}")
-
-        return String(take!(buf))
     end
 end
 
@@ -70,7 +23,11 @@ function (v::Voyager)(source)
 
     it = IteratorInterfaceExtensions.getiterator(source)
 
-    data = format_iterable_table_as_json(it)
+    data_dict = Dict()
+
+    data_dict["values"] = [Dict(c[1]=>isnull(c[2]) ? nothing : get(c[2]) for c in zip(keys(r), values(r))) for r in it]
+
+    data = JSON.json(data_dict)
 
     jsdata = Blink.JSString(data)
 
